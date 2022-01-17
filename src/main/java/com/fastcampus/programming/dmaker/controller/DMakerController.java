ff70e2dff0ced8d4f604e66bef9c2291dad8a356
bangddong <gusehdqkd@naver.com>
2022-01-17 22:13:47 +0900

    커스텀 에러 Response 추가 DMakerController Exception Handler 추가

--- src/main/java/com/fastcampus/programming/dmaker/controller/DMakerController.java
+++ src/main/java/com/fastcampus/programming/dmaker/controller/DMakerController.java
@@ -1,14 +1,14 @@
 package com.fastcampus.programming.dmaker.controller;
 
-import com.fastcampus.programming.dmaker.dto.CreateDeveloper;
-import com.fastcampus.programming.dmaker.dto.DeveloperDetailDto;
-import com.fastcampus.programming.dmaker.dto.DeveloperDto;
-import com.fastcampus.programming.dmaker.dto.EditDeveloper;
+import com.fastcampus.programming.dmaker.dto.*;
+import com.fastcampus.programming.dmaker.exception.DMakerException;
 import com.fastcampus.programming.dmaker.service.DMakerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.List;
 
@@ -58,5 +58,20 @@ public class DMakerController {
             @PathVariable String memberId
     ) {
         return dMakerService.deleteDeveloper(memberId);
+    }
+
+    /**
+     * DMakerController 내에서 발생하는 DMakerException 을 핸들링
+     */
+    @ResponseStatus(value = HttpStatus.CONFLICT)
+    @ExceptionHandler(DMakerException.class)
+    public DMakerErrorResponse handleException (DMakerException e, HttpServletRequest request) {
+        log.error("errorCode: {}, url: {}, message: {}",
+                e.getDMakerErrorCde(), request.getRequestURI(), e.getDetailMessage());
+
+        return DMakerErrorResponse.builder()
+                .errorCode(e.getDMakerErrorCde())
+                .errorMessage(e.getDetailMessage())
+                .build();
     }
 }
